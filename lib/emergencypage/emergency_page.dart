087cbe734b89f967e1fd3d7f9c4ba3023a30ce88
Sylvia Thornton <s_thornton1@u.pacific.edu>
2024-05-04 13:31:04 -0700

    emergencyPageAdded

--- lib/emergencypage/emergency_page.dart
+++ lib/emergencypage/emergency_page.dart
@@ -3,8 +3,6 @@ import 'package:flutter_playground/bottom_nav_bar.dart';
 import 'package:flutter_playground/homepage/title_section.dart';
 import 'package:flutter_playground/homepage/wave_header.dart';
 
-
-
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key? key}) : super(key: key);
 
@@ -23,10 +21,47 @@ class EmergencyPage extends StatelessWidget {
             stops: const [0.0, 1.0],
           ),
         ),
-        child: const Column(
+        child: Column(
           children: [
-            WaveHeader(),
-            TitleSection(),
+            const WaveHeader(),
+            const TitleSection(),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: const Text(
+                "CONTACTING EMERGENCY RESCUE SERVICES...",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.red,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: const Text(
+                "While you wait",
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Color.fromARGB(255, 68, 113, 167),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            // Adding an image from assets
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset(
+                'assets/images/Emergencycard1.png',  // Replace with your asset path
+                fit: BoxFit.cover, // Adjust the fit as needed
+              ),
+            ),
+           Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset(
+                'assets/images/Emergencycard2.png',  // Replace with your asset path
+                fit: BoxFit.cover, // Adjust the fit as needed
+              ),
+            ),
           ],
         ),
       ),
